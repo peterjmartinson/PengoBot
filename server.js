431efeaaf4c76a5ebafd15b896a562a377612e70
Khyati Kulshreshtha <khyati.kul@gmail.com>
2017-03-09 22:29:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,22 @@
 //dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+//global config
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/',function(req,res){
+
+});
+
+app.post('/:id', function(req,res){
+
+});
+app.listen(PORT, function(){
+	console.log('Server is listening to %d port in %s mode',PORT,app.settings.env);
+});
