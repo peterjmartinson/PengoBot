21b276778c0fe80155ee3cf4d1abc7919e96ed83
Monyett <monyett.tanzillo@gmail.com>
2017-03-13 13:46:54 -0400

    Add db connection and configure dotenv

--- server.js
+++ server.js
@@ -3,8 +3,18 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 const app = express();
 //const pengo = require('./pengo');
+
+var url = process.env.MONGOLAB_URI;
+mongoose.connect(url);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('Db connected successfully');
+});
 
 //global config
 var PORT = process.env.PORT || 3000;
