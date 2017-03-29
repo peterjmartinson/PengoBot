806de69c31b910f5cf32feca6df5a912390ee0ca
Peter Martinson <peter.j.martinson@gmail.com>
2017-03-28 21:56:32 -0400

    Add getCommand.js
    Adds webscraping and result parsing abilities.

---
+++ app/getCommand.js
@@ -0,0 +1,64 @@
+/* 
+ * getCommand(command, callback)
+ * 
+ * pass a Bash command to getCommand, and receive
+ * the main parts of the relevant man page!
+ * 
+ * Author: Peter Martinson
+ * Date:   March 27, 2017
+*/
+
+
+const request = require('request'),
+      cheerio = require('cheerio'),
+      baseURL = 'http://man.he.net/man1/';
+
+// get the command text from the URL
+var fetchCommand = function(command, callback) {
+  var mainURL = baseURL + command;
+  request(mainURL, function(error, response, html) {
+    if (error) callback(error);
+    var $ = cheerio.load(html);
+    callback(null, $('pre').html());
+  });
+}
+
+module.exports = function(command, callback) {
+  fetchCommand(command, function(error, man) {
+    if (error) callback(error);
+
+    var commandArray = man.split('\n'),
+        toc          = [],
+        sections     = [];
+
+    // create TOC numbers
+    commandArray.forEach(function(element, index) {
+      if ( /^[A-Z]/.test(element) ) {
+        toc.push(index);
+      }
+    });
+
+    // create TOC section labels
+    toc.forEach(function(element) {
+      sections.push(commandArray[element]);
+    });
+
+    function createSection(title) {
+      return commandArray.filter(function(element, index) {
+        var sectionIndex    = toc[sections.indexOf(title.toUpperCase())];
+        var nextSectionIndex = toc[sections.indexOf(title.toUpperCase())+1];
+        if ( index > sectionIndex && index < nextSectionIndex ) {
+          return element;
+        }
+      }).join('\n');
+    }
+
+    callback(null, {
+      synopsis    : createSection('synopsis'),
+      description : createSection('description'),
+      options     : createSection('options'),
+      examples    : createSection('examples')
+    });
+  });
+}
+
